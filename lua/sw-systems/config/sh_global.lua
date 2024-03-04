d0173c974c371eb9a522197a5892371017919f8b
senfauge <martsenf@web.de>
2024-03-04 19:48:33 +0100

    added security to the reactpr terminal

--- lua/sw-systems/config/sh_global.lua
+++ lua/sw-systems/config/sh_global.lua
@@ -2,3 +2,16 @@ SWS = SWS or {}
 SWS.ENUM = SWS.ENUM or {}
 
 SWS.ENTITY_CATEGORY = "[SW: Systems] "
+
+-- the minimum range for entities to be interactible
+SWS.ENTITY_RANGE = 8000
+
+SWS.ADMIN_GROUPS = {}
+SWS.ADMIN_GROUPS["superadmin"] = true
+SWS.ADMIN_GROUPS["admin"] = true
+SWS.ADMIN_GROUPS["owner"] = true
+
+-- do your own permission checks here if needed
+function SWS.IsAdmin(ply)
+    return SWS.ADMIN_GROUPS[ply:GetUserGroup()]
+end
--- lua/sw-systems/systems/reactor/sv_reactor.lua
+++ lua/sw-systems/systems/reactor/sv_reactor.lua
@@ -5,7 +5,7 @@ SWS.Reactor = SWS.Reactor or {}
 function SWS.Reactor:SetStatus(status)
     SWS.Reactor.status = math.Clamp(status, 1, table.Count(SWS.ENUM.REACTOR_STATUS))
     net.Start("SWS.Reactor.Status")
-        net.WriteUInt(SWS.Reactor.status, 4)
+        net.WriteUInt(SWS.Reactor:GetStatus(), 4)
     net.Broadcast()
 end
 
@@ -13,21 +13,21 @@ function SWS.Reactor:SetPowerOutput(number)
     if SWS.Reactor:GetStatus() == SWS.ENUM.REACTOR_STATUS.MELTDOWN then return end
     SWS.Reactor.powerOutput = math.Clamp(number, 0, SWS.Reactor.MAX_POWER_OUTPUT)
     net.Start("SWS.Reactor.Power")
-        net.WriteUInt(SWS.Reactor.powerOutput, 8)
+        net.WriteUInt(SWS.Reactor:GetPowerOutput(), 8)
     net.Broadcast()
 end
 
 function SWS.Reactor:SetCoolingPower(number)
     SWS.Reactor.coolingPower = math.Clamp(number, 0, SWS.Reactor.MAX_COOLING_POWER)
     net.Start("SWS.Reactor.Cooling")
-        net.WriteUInt(SWS.Reactor.coolingPower, 8)
+        net.WriteUInt(SWS.Reactor:GetCoolingPower(), 8)
     net.Broadcast()
 end
 
 function SWS.Reactor:SetHeat(number)
     SWS.Reactor.heat = math.Clamp(number, 0, SWS.Reactor.MAX_HEAT)
     net.Start("SWS.Reactor.Heat")
-        net.WriteUInt(SWS.Reactor.heat, 8)
+        net.WriteUInt(SWS.Reactor:GetHeat(), 8)
     net.Broadcast()
 end
 
@@ -58,26 +58,36 @@ util.AddNetworkString("SWS.Reactor.Cooling")
 util.AddNetworkString("SWS.Reactor.Heat")
 util.AddNetworkString("SWS.Reactor.SyncData")
 
+local function terminalInRange(ply, class)
+    for _, v in ipairs(ents.FindByClass(class)) do
+        if v:GetPos():DistToSqr(ply:GetPos()) <= SWS.ENTITY_RANGE then
+            return true
+        end
+    end
+end
+
 net.Receive("SWS.Reactor.Power", function(len, ply)
-    -- todo: add security
-    SWS.Reactor:SetPowerOutput(net.ReadUInt(8))
+    if SWS.IsAdmin(ply) or terminalInRange(ply, "sws_reactor_terminal") then
+        SWS.Reactor:SetPowerOutput(net.ReadUInt(8))
+    end
 end)
 
 net.Receive("SWS.Reactor.Cooling", function(len, ply)
-    -- todo: add security
-    SWS.Reactor:SetCoolingPower(net.ReadUInt(8))
+    if SWS.IsAdmin(ply) or terminalInRange(ply, "sws_reactor_terminal") then
+        SWS.Reactor:SetCoolingPower(net.ReadUInt(8))
+    end
 end)
 
 hook.Add("SWS.PlayerLoaded", "SWS.Reactor.SyncData", function(ply)
     net.Start("SWS.Reactor.SyncData")
-        net.WriteUInt(SWS.Reactor.status, 4)
-        net.WriteUInt(SWS.Reactor.powerOutput, 8)
-        net.WriteUInt(SWS.Reactor.coolingPower, 8)
-        net.WriteUInt(SWS.Reactor.heat, 8)
-    net.Broadcast()
+        net.WriteUInt(SWS.Reactor:GetStatus(), 4)
+        net.WriteUInt(SWS.Reactor:GetPowerOutput(), 8)
+        net.WriteUInt(SWS.Reactor:GetCoolingPower(), 8)
+        net.WriteUInt(SWS.Reactor:GetHeat(), 8)
+    net.Send(ply)
 end)
 
-
+-- todo: rework into command or automation on cleanup
 concommand.Add("reset_reactor", function(ply, cmd)
     SWS.Reactor:SetStatus(1)
     SWS.Reactor:SetPowerOutput(0)
