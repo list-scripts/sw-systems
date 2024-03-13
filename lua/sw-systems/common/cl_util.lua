828f0f07c7252abf6004c8c92a3d0f023c698b0d
Luiggi33 <luiggi33@web.de>
2024-03-13 21:43:57 +0100

    comment out unused hook

--- lua/sw-systems/common/cl_util.lua
+++ lua/sw-systems/common/cl_util.lua
@@ -29,12 +29,13 @@ local function drawHorizontalStripedRect(x, y, w, h, color1, color2, thickness)
     local remainder = w%thickness
 
     surface.DrawRect(x + w - remainder, y, remainder, h)
-end]]
+end
 
 hook.Add("HUDPaint", "Test", function()
-    --drawVerticalStripedRect(100, 100, 60, 200, Color(255, 255, 255), Color(0, 0, 0), 4)
-    --drawHorizontalStripedRect(200, 100, 200, 60, Color(255, 255, 255), Color(0, 0, 0), 4)
+    drawVerticalStripedRect(100, 100, 60, 200, Color(255, 255, 255), Color(0, 0, 0), 4)
+    drawHorizontalStripedRect(200, 100, 200, 60, Color(255, 255, 255), Color(0, 0, 0), 4)
 end)
+]]
 
 function draw.SpacedText(text, font, x, y, color, spacing)
     surface.SetFont(font)
